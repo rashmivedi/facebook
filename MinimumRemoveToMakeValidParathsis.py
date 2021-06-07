f1b5499b2c591df93ce50e53bf1b301abae1ff22
rashmivedi <78020213+rashmivedi@users.noreply.github.com>
2021-06-07 09:37:28 +0530

    Create MinimumRemoveToMakeValidParathsis.py

---
+++ MinimumRemoveToMakeValidParathsis.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def minRemoveToMakeValid(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # Pass 1: Remove all invalid ")"
+        first_pass_chars = []
+        balance = 0
+        open_seen = 0
+        for c in s:
+            if c == "(":
+                balance += 1
+                open_seen += 1
+            if c == ")":
+                if balance == 0:
+                    continue
+                balance -= 1
+            first_pass_chars.append(c)
+
+        # Pass 2: Remove the rightmost "("
+        result = []
+        open_to_keep = open_seen - balance
+        for c in first_pass_chars:
+            if c == "(":
+                open_to_keep -= 1
+                if open_to_keep < 0:
+                    continue
+            result.append(c)
+
+        return "".join(result)
